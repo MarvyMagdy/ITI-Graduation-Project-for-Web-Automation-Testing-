daa382c49d848d529f197fc7d8b1194ec7913cad
MarvyMagdy <98482215+MarvyMagdy@users.noreply.github.com>
2022-02-24 23:49:13 +0200

    Add files via upload

--- src/test/java/register/RegisterTests.java
+++ src/test/java/register/RegisterTests.java
@@ -1,1 +1,29 @@
+package register;
 
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import Bank_Pages.LoginPage;
+import Bank_Pages.RegisterationCompletionPage;
+import Bank_Pages.RegisterationPage;
+import Project.ParaBank.BankBaseTests;
+
+public class RegisterTests extends BankBaseTests 
+{
+	
+	
+	
+	@Test
+	public void RegisterSuccessfully()
+	{
+		LoginPage loginpage = new LoginPage(driver);
+		
+		RegisterationPage registeratiopagePage = loginpage.ClickRegisterButton();
+		registeratiopagePage.SetRegisterationData("AA", "AA", "AA", "AA", "AA", "AA", "AA", "AA", user, pass, pass);
+		RegisterationCompletionPage registerComplete = registeratiopagePage.ClickRegisterButton();
+		assertTrue(registerComplete.GetWelcomeMsg().contains("Welcome"),"Failed To register");
+		
+	}
+
+}
