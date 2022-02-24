d075cddb4314e8f7c21ad1cbed4cbf8c4452caa4
MarvyMagdy <98482215+MarvyMagdy@users.noreply.github.com>
2022-02-24 23:50:19 +0200

    Add files via upload

--- src/test/java/transferfund/TransferFundTests.java
+++ src/test/java/transferfund/TransferFundTests.java
@@ -1,1 +1,28 @@
+package transferfund;
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
 
+import Bank_Pages.Home;
+import Bank_Pages.TransferCompletePage;
+import Bank_Pages.TransferFundsPage;
+import Project.ParaBank.BankBaseTests;
+import loan.LoanTests;
+
+
+public class TransferFundTests extends BankBaseTests 
+{
+	
+	@Test
+	public void TransferfundSuccessfully()
+	{
+		Home homepage = new Home(driver);
+		
+		TransferFundsPage transferpage = homepage.ClickTransferfundsLink();
+	
+		transferpage.SelectFromDropdownrecieve(LoanTests.acc);
+		transferpage.SetAmount("9");
+		TransferCompletePage transfercompletion = transferpage.ClickTransferButton();
+		assertTrue(transfercompletion.GetCompletionMsg().contains("Transfer"),"Transfer Failed !!");
+	}
+	
+}
