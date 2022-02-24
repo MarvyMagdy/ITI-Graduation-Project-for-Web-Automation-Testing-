c8700b69d116475f3113529c906e71dd1427423b
MarvyMagdy <98482215+MarvyMagdy@users.noreply.github.com>
2022-02-24 23:52:35 +0200

    Add files via upload

--- src/test/java/Update_Account/UPdateAccountTests.java
+++ src/test/java/Update_Account/UPdateAccountTests.java
@@ -1,1 +1,25 @@
+package Update_Account;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import Bank_Pages.Home;
+import Bank_Pages.UpdateContactInfo;
+import Project.ParaBank.BankBaseTests;
+
+public class UPdateAccountTests extends BankBaseTests
+{
+	@Test
+	public void updateAccountFNameTest()
+	{
+		Home homepage = new Home(driver);
+		
+		UpdateContactInfo updateContactInfo = homepage.clickUpdateContactInfo();
+		
+		updateContactInfo.UpdateInfo("Marvy");
+		updateContactInfo.clickUpdateBTN();
+		
+		assertEquals(updateContactInfo.getUpdatedComplete(), "Update Profile");
+	}
+}
