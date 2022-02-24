231b3e9e2e4faf4276ad4ade523d38170244406a
MarvyMagdy <98482215+MarvyMagdy@users.noreply.github.com>
2022-02-24 23:47:01 +0200

    Add files via upload

--- src/test/java/New_Account/OpenNewAccountTests.java
+++ src/test/java/New_Account/OpenNewAccountTests.java
@@ -1,1 +1,17 @@
+package New_Account;
 
+import org.testng.annotations.Test;
+
+import Bank_Pages.Home;
+import Project.ParaBank.BankBaseTests;
+
+public class OpenNewAccountTests extends BankBaseTests
+{
+	@Test
+	public void openNewAccountTest()
+	{
+		Home homepage = new Home(driver);
+		
+		homepage.clickOpenNewAccount().AccountType("SAVINGS").clickOpenNewAccountBTN();
+	}
+}
