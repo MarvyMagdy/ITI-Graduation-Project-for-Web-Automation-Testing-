d30023d0cc582aab66611932623647ff3004f266
MarvyMagdy <98482215+MarvyMagdy@users.noreply.github.com>
2022-02-24 23:46:01 +0200

    Add files via upload

--- src/test/java/Logout/LogoutTests.java
+++ src/test/java/Logout/LogoutTests.java
@@ -1,1 +1,17 @@
+package Logout;
 
+import org.testng.annotations.Test;
+
+import Bank_Pages.Home;
+import Project.ParaBank.BankBaseTests;
+
+public class LogoutTests extends BankBaseTests
+{
+	@Test
+	public void logoutCheck()
+	{
+		Home homepage = new Home(driver);
+		
+		homepage.clickLogout();
+	}
+}
